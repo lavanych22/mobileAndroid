88b2e980091f66bbc13e7d26d25b61e8dd211194
Ivan Dovhan <letsplayclash15@gmail.com>
2020-01-27 14:01:14 +0200

    added lab3

---
+++ app/src/main/java/com/dovhan/mobileapp/main_group/about/AboutFragment.java
@@ -0,0 +1,30 @@
+package com.dovhan.mobileapp.main_group.about;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.dovhan.mobileapp.R;
+
+public class AboutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        TextView author = view.findViewById(R.id.author);
+
+        author.setText("Ivan Dovhan");
+
+        return view;
+    }
+}
