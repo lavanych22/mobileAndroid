bef474612b01a8ac0cdb170b7a3edbc4ffd846a8
Ivan Dovhan <letsplayclash15@gmail.com>
2020-01-27 14:09:38 +0200

    added lab5

---
+++ app/src/main/java/com/dovhan/mobileapp/utils/JSONParser.java
@@ -0,0 +1,40 @@
+package com.dovhan.mobileapp.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.dovhan.mobileapp.data.model.Car;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONParser {
+
+    private JSONParser() {
+        //default empty constructor for Singleton class with no attributes
+    }
+
+    public static <T> List<T> getFromJSONtoArrayList(String jsonString, Type type) {
+        if (!isValid(jsonString))
+            return new ArrayList<>();
+        return new Gson().fromJson(jsonString, type);
+    }
+
+    public static Car getFromJSONToCar(String json, Type type) {
+        if (!isValid(json)) {
+            return null;
+        } else {
+            return new Gson().fromJson(json, type);
+        }
+    }
+
+    private static boolean isValid(String jsonString) {
+        try {
+            new JsonParser().parse(jsonString);
+            return true;
+        } catch (JsonSyntaxException e) {
+            return false;
+        }
+    }
+}
